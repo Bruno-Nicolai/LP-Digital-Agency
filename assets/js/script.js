fa17cd0f97703a6089ee1d40003737e58f5d3e84
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2025-08-10 14:08:56 -0300

    feat: interactive button

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Immediately Invoked Function Expression
+(() => {
+  /* navbar */
+  const navbar = document.getElementById("nav");
+  const toggleMenuButton = document.getElementById("navbar-toggle");
+  const ionicon = toggleMenuButton.querySelector("ion-icon");
+
+  if (ionicon && toggleMenuButton && navbar) {
+    toggleMenuButton.addEventListener("click", () => {
+      const isActive = navbar.classList.toggle("active");
+      ionicon.setAttribute("name", isActive ? "close" : "chevron-down");
+    });
+  }
+})();
